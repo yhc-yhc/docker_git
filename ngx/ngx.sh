fb041f85dc8755ce7013d3584fb969edb8bf206f
josh <josh.zhu@pictureworks.biz>
2017-05-15 18:46:13 +0800

    josh

--- ngx/ngx.sh
+++ ngx/ngx.sh
@@ -9,7 +9,6 @@ cp -r ~/work/docker_git/ngx/ngx_demo ~/work/ngx_demo
 docker run \
 --privileged=true -v /etc/localtime:/etc/localtime \
 -v ~/ngx_conf:/etc/nginx/conf.d \
-
 -v ~/work/ngx_demo/demo:/www \
 -p 80:80 \
 --name ngx -d nginx
