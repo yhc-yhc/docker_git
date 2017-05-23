3a670a9dee1b7e84a90eb74e8c58e388c90db73d
josh <josh.zhu@pictureworks.biz>
2017-05-23 17:45:01 +0800

    josh

--- ngx/ngx.sh
+++ ngx/ngx.sh
@@ -14,7 +14,8 @@ docker rm ngx;
 docker run \
 --privileged=true -v /etc/localtime:/etc/localtime \
 -v ~/work/ngx_log:/usr/local/openresty/nginx/logs \
--v ~/work/docker_git/ngx/ngx_conf:/usr/local/openresty/nginx/conf/nginx.conf  \
+-v ~/work/docker_git/ngx/ngix.conf:/usr/local/openresty/nginx/conf/nginx.conf:ro  \
+-v ~/work/docker_git/ngx/ngx_conf:/etc/nginx/conf.d
 -v ~/work/pictureAir/static:/www \
 -p 80:80 \
 --name ngx -d openresty/openresty:1.9.15.1-trusty
@@ -25,3 +26,9 @@ docker run \
 # -v $PWD/config/nginx.conf:/usr/local/openresty/nginx/conf/nginx.conf:ro 
 # -v $PWD/logs:/usr/local/openresty/nginx/logs 
 # openresty/openresty:1.9.15.1-trusty
+
+
+docker run -d --name ngx --net=host -v $PWD/config/nginx.conf:/usr/local/openresty/nginx/conf/nginx.conf:ro openresty/openresty
+
+
+docker run -d --name ngx -p 8080:80 --privileged=true -v ~/work/config/nginx.conf:/usr/local/openresty/nginx/conf/nginx.conf:ro openresty/openresty
