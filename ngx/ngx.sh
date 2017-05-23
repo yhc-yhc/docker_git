5cd49fd5a059a5d72e45a627aa0963f5ef766c77
josh <josh.zhu@pictureworks.biz>
2017-05-23 17:54:30 +0800

    josh

--- ngx/ngx.sh
+++ ngx/ngx.sh
@@ -14,7 +14,7 @@ docker rm ngx;
 docker run \
 --privileged=true -v /etc/localtime:/etc/localtime \
 -v ~/work/ngx_log:/usr/local/openresty/nginx/logs \
--v ~/work/docker_git/ngx/ngix.conf:/usr/local/openresty/nginx/conf/nginx.conf:ro  \
+-v ~/work/docker_git/ngx/nginx.conf:/usr/local/openresty/nginx/conf/nginx.conf:ro  \
 -v ~/work/docker_git/ngx/ngx_conf:/etc/nginx/conf.d \
 -v ~/work/pictureAir/static:/www \
 -p 80:80 \
