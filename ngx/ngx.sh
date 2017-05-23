9467d7385756edd047353cb276d22685b15d2ce9
josh <josh.zhu@pictureworks.biz>
2017-05-23 16:23:20 +0800

    josh

--- ngx/ngx.sh
+++ ngx/ngx.sh
@@ -3,7 +3,7 @@ cd ~
 mkdir work || { echo work dir exists; }
 cd work
 
-cp -r ~/work/docker_git/ngx/ngx_conf ~/work/ngx_conf
+# cp -r ~/work/docker_git/ngx/ngx_conf ~/work/ngx_conf
 # cp -r ~/work/docker_git/ngx/ngx_demo ~/work/ngx_demo
 
 
@@ -13,7 +13,7 @@ docker rm ngx;
 docker run \
 --privileged=true -v /etc/localtime:/etc/localtime \
 -v ~/work/ngx_log:/var/log/nginx \
--v ~/work/ngx_conf:/etc/nginx/conf.d \
+-v ~/work/docker_git/ngx/ngx_conf:/etc/nginx/conf.d \
 -v ~/work/pictureAir/static:/www \
 -p 80:80 \
 --name ngx -d nginx
