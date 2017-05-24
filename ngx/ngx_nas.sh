b4e8d7d67090f28f7bf37d991176ee63614c6130
josh <josh.zhu@pictureworks.biz>
2017-05-24 15:54:29 +0800

    josh

---
+++ ngx/ngx_nas.sh
@@ -0,0 +1,21 @@
+cd ~
+
+mkdir work || { echo work dir exists; }
+cd work
+
+# cp -r ~/work/docker_git/ngx/ngx_conf ~/work/ngx_conf
+# cp -r ~/work/docker_git/ngx/ngx_demo ~/work/ngx_demo
+
+# rm -rf ngx_log
+
+docker stop ngx || echo ngx container not exists;
+docker rm ngx || echo ngx container not exists;
+
+docker run \
+--privileged=true -v /etc/localtime:/etc/localtime \
+-v ~/work/ngx_log:/usr/local/openresty/nginx/logs \
+-v ~/work/docker_git/ngx/nginx.conf:/usr/local/openresty/nginx/conf/nginx.conf:ro  \
+-v ~/work/docker_git/ngx/ngx_conf/nas:/usr/local/openresty/nginx/conf/conf.d \
+-v /mnt/photos:/www \
+-p 80:80 \
+--name ngx -d openresty/openresty
--- ngx/ngx_web.sh
+++ ngx/ngx_web.sh
@@ -15,7 +15,7 @@ docker run \
 --privileged=true -v /etc/localtime:/etc/localtime \
 -v ~/work/ngx_log:/usr/local/openresty/nginx/logs \
 -v ~/work/docker_git/ngx/nginx.conf:/usr/local/openresty/nginx/conf/nginx.conf:ro  \
--v ~/work/docker_git/ngx/ngx_conf:/usr/local/openresty/nginx/conf/conf.d \
+-v ~/work/docker_git/ngx/ngx_conf/web:/usr/local/openresty/nginx/conf/conf.d \
 -v ~/work/pictureAir/static:/www \
 -p 80:80 \
 --name ngx -d openresty/openresty
