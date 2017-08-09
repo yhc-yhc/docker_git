dcd11b55f764365ce936abcceaa2a5cf850886ae
josh <josh.zhu@pictureworks.biz>
2017-08-09 14:12:49 +0800

    add mongo dump and restor

--- redis/redis.sh
+++
@@ -1,4 +0,0 @@
-docker run \
---privileged=true -v /etc/localtime:/etc/localtime \
--p 830:6379 \
---name redis -d redis:3.2.8
---
+++ redis/redis_mongo.sh
@@ -0,0 +1,14 @@
+docker rm -f redis;
+docker run \
+--privileged=true -v /etc/localtime:/etc/localtime \
+-p 8300:6379 \
+--name redis -d redis:3.2.8
+
+docker rm -f mongodb;
+docker run \
+--privileged=true -v /etc/localtime:/etc/localtime \
+-v ~/work/docker_git/redis:/src
+-p 8827:27017 \
+--name mongodb -d mongo:3.2 /bin/bash -C \ 
+"mongod --dbpath /data/db  --replSet pictureAir --oplogSize 128 --storageEngine wiredTiger; mongorestor -d pictureAir --dir /src"
+
