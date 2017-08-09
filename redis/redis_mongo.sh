7f41d6c4d27f24cd9673042971ef3fe7b213786e
josh <josh.zhu@pictureworks.biz>
2017-08-09 14:18:22 +0800

    add mongo dump and restor

--- redis/redis_mongo.sh
+++ redis/redis_mongo.sh
@@ -7,7 +7,7 @@ docker run \
 docker rm -f mongodb;
 docker run \
 --privileged=true -v /etc/localtime:/etc/localtime \
--v ~/work/docker_git/redis:/src
+-v ~/work/docker_git/redis/pictureAir:/src
 -p 8827:27017 \
 --name mongodb -d mongo:3.2 /bin/bash -C \ 
 "mongod --dbpath /data/db  --replSet pictureAir --oplogSize 128 --storageEngine wiredTiger; mongorestor -d pictureAir --dir /src"
